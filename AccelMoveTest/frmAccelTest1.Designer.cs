1c1d5e08d902b387eb567f74d4154b1aab6f5a23
miguel28 <miguelinho_28@hotmail.com>
2014-08-12 21:02:45 -0600

    Calibration Controls Support

--- AccelMoveTest/frmAccelTest1.Designer.cs
+++ AccelMoveTest/frmAccelTest1.Designer.cs
@@ -41,38 +41,47 @@
             SpPerfChart.ChartPen chartPen10 = new SpPerfChart.ChartPen();
             SpPerfChart.ChartPen chartPen11 = new SpPerfChart.ChartPen();
             SpPerfChart.ChartPen chartPen12 = new SpPerfChart.ChartPen();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
+            this.tmrReceive = new System.Windows.Forms.Timer(this.components);
+            this.lblRawValues = new System.Windows.Forms.Label();
             this.perfChart1 = new SpPerfChart.PerfChart();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.grpAxisX = new System.Windows.Forms.GroupBox();
+            this.grpAxisY = new System.Windows.Forms.GroupBox();
             this.perfChart2 = new SpPerfChart.PerfChart();
+            this.grpAxisZ = new System.Windows.Forms.GroupBox();
             this.perfChart3 = new SpPerfChart.PerfChart();
+            this.grpCommands = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnCalibrate = new System.Windows.Forms.Button();
+            this.lblXOffset = new System.Windows.Forms.Label();
+            this.lblYOffset = new System.Windows.Forms.Label();
+            this.lblZOffset = new System.Windows.Forms.Label();
+            this.lblPosZ = new System.Windows.Forms.Label();
+            this.lblPosY = new System.Windows.Forms.Label();
+            this.lblPosX = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
+            this.grpAxisX.SuspendLayout();
+            this.grpAxisY.SuspendLayout();
+            this.grpAxisZ.SuspendLayout();
+            this.grpCommands.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // timer1
+            // tmrReceive
             // 
-            this.timer1.Interval = 20;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.tmrReceive.Interval = 20;
+            this.tmrReceive.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // label1
+            // lblRawValues
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 571);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 31);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblRawValues.AutoSize = true;
+            this.lblRawValues.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRawValues.Location = new System.Drawing.Point(3, 571);
+            this.lblRawValues.Name = "lblRawValues";
+            this.lblRawValues.Size = new System.Drawing.Size(86, 31);
+            this.lblRawValues.TabIndex = 0;
+            this.lblRawValues.Text = "label1";
             // 
             // perfChart1
             // 
@@ -113,10 +122,10 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.groupBox4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.grpAxisX, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.grpAxisY, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.grpAxisZ, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.grpCommands, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -126,64 +135,27 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(927, 565);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
-            // groupBox1
+            // grpAxisX
             // 
-            this.groupBox1.Controls.Add(this.perfChart1);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(457, 276);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Axis X";
+            this.grpAxisX.Controls.Add(this.perfChart1);
+            this.grpAxisX.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpAxisX.Location = new System.Drawing.Point(3, 3);
+            this.grpAxisX.Name = "grpAxisX";
+            this.grpAxisX.Size = new System.Drawing.Size(457, 276);
+            this.grpAxisX.TabIndex = 0;
+            this.grpAxisX.TabStop = false;
+            this.grpAxisX.Text = "Axis X";
             // 
-            // groupBox2
+            // grpAxisY
             // 
-            this.groupBox2.Controls.Add(this.perfChart2);
-            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(466, 3);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(458, 276);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Axis Y";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.perfChart3);
-            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox3.Location = new System.Drawing.Point(3, 285);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(457, 277);
-            this.groupBox3.TabIndex = 2;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Axis Z";
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox4.Location = new System.Drawing.Point(466, 285);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(458, 277);
-            this.groupBox4.TabIndex = 3;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "groupBox4";
-            // 
-            // tableLayoutPanel2
-            // 
-            this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 1);
-            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
-            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(933, 621);
-            this.tableLayoutPanel2.TabIndex = 3;
+            this.grpAxisY.Controls.Add(this.perfChart2);
+            this.grpAxisY.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpAxisY.Location = new System.Drawing.Point(466, 3);
+            this.grpAxisY.Name = "grpAxisY";
+            this.grpAxisY.Size = new System.Drawing.Size(458, 276);
+            this.grpAxisY.TabIndex = 1;
+            this.grpAxisY.TabStop = false;
+            this.grpAxisY.Text = "Axis Y";
             // 
             // perfChart2
             // 
@@ -219,6 +191,17 @@
             this.perfChart2.TimerInterval = 20;
             this.perfChart2.TimerMode = SpPerfChart.TimerMode.Disabled;
             // 
+            // grpAxisZ
+            // 
+            this.grpAxisZ.Controls.Add(this.perfChart3);
+            this.grpAxisZ.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpAxisZ.Location = new System.Drawing.Point(3, 285);
+            this.grpAxisZ.Name = "grpAxisZ";
+            this.grpAxisZ.Size = new System.Drawing.Size(457, 277);
+            this.grpAxisZ.TabIndex = 2;
+            this.grpAxisZ.TabStop = false;
+            this.grpAxisZ.Text = "Axis Z";
+            // 
             // perfChart3
             // 
             this.perfChart3.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -253,6 +236,114 @@
             this.perfChart3.TimerInterval = 20;
             this.perfChart3.TimerMode = SpPerfChart.TimerMode.Disabled;
             // 
+            // grpCommands
+            // 
+            this.grpCommands.Controls.Add(this.btnStart);
+            this.grpCommands.Controls.Add(this.lblPosZ);
+            this.grpCommands.Controls.Add(this.lblPosY);
+            this.grpCommands.Controls.Add(this.lblPosX);
+            this.grpCommands.Controls.Add(this.lblZOffset);
+            this.grpCommands.Controls.Add(this.lblYOffset);
+            this.grpCommands.Controls.Add(this.lblXOffset);
+            this.grpCommands.Controls.Add(this.btnCalibrate);
+            this.grpCommands.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpCommands.Location = new System.Drawing.Point(466, 285);
+            this.grpCommands.Name = "grpCommands";
+            this.grpCommands.Size = new System.Drawing.Size(458, 277);
+            this.grpCommands.TabIndex = 3;
+            this.grpCommands.TabStop = false;
+            this.grpCommands.Text = "Commands";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.lblRawValues, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(933, 621);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // btnCalibrate
+            // 
+            this.btnCalibrate.Location = new System.Drawing.Point(18, 19);
+            this.btnCalibrate.Name = "btnCalibrate";
+            this.btnCalibrate.Size = new System.Drawing.Size(80, 26);
+            this.btnCalibrate.TabIndex = 0;
+            this.btnCalibrate.Text = "Calibrate";
+            this.btnCalibrate.UseVisualStyleBackColor = true;
+            this.btnCalibrate.Click += new System.EventHandler(this.btnCalibrate_Click);
+            // 
+            // lblXOffset
+            // 
+            this.lblXOffset.AutoSize = true;
+            this.lblXOffset.Location = new System.Drawing.Point(18, 66);
+            this.lblXOffset.Name = "lblXOffset";
+            this.lblXOffset.Size = new System.Drawing.Size(35, 13);
+            this.lblXOffset.TabIndex = 1;
+            this.lblXOffset.Text = "label1";
+            // 
+            // lblYOffset
+            // 
+            this.lblYOffset.AutoSize = true;
+            this.lblYOffset.Location = new System.Drawing.Point(18, 95);
+            this.lblYOffset.Name = "lblYOffset";
+            this.lblYOffset.Size = new System.Drawing.Size(35, 13);
+            this.lblYOffset.TabIndex = 2;
+            this.lblYOffset.Text = "label2";
+            // 
+            // lblZOffset
+            // 
+            this.lblZOffset.AutoSize = true;
+            this.lblZOffset.Location = new System.Drawing.Point(18, 121);
+            this.lblZOffset.Name = "lblZOffset";
+            this.lblZOffset.Size = new System.Drawing.Size(35, 13);
+            this.lblZOffset.TabIndex = 3;
+            this.lblZOffset.Text = "label3";
+            // 
+            // lblPosZ
+            // 
+            this.lblPosZ.AutoSize = true;
+            this.lblPosZ.Location = new System.Drawing.Point(181, 121);
+            this.lblPosZ.Name = "lblPosZ";
+            this.lblPosZ.Size = new System.Drawing.Size(35, 13);
+            this.lblPosZ.TabIndex = 6;
+            this.lblPosZ.Text = "label3";
+            // 
+            // lblPosY
+            // 
+            this.lblPosY.AutoSize = true;
+            this.lblPosY.Location = new System.Drawing.Point(181, 95);
+            this.lblPosY.Name = "lblPosY";
+            this.lblPosY.Size = new System.Drawing.Size(35, 13);
+            this.lblPosY.TabIndex = 5;
+            this.lblPosY.Text = "label2";
+            // 
+            // lblPosX
+            // 
+            this.lblPosX.AutoSize = true;
+            this.lblPosX.Location = new System.Drawing.Point(181, 66);
+            this.lblPosX.Name = "lblPosX";
+            this.lblPosX.Size = new System.Drawing.Size(35, 13);
+            this.lblPosX.TabIndex = 4;
+            this.lblPosX.Text = "label1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(136, 19);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 26);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
             // frmAccelTest1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -262,9 +353,11 @@
             this.Name = "frmAccelTest1";
             this.Text = "Accelerometer Test";
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
+            this.grpAxisX.ResumeLayout(false);
+            this.grpAxisY.ResumeLayout(false);
+            this.grpAxisZ.ResumeLayout(false);
+            this.grpCommands.ResumeLayout(false);
+            this.grpCommands.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
@@ -273,17 +366,25 @@
 
         #endregion
 
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer tmrReceive;
+        private System.Windows.Forms.Label lblRawValues;
         private SpPerfChart.PerfChart perfChart1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox grpAxisX;
+        private System.Windows.Forms.GroupBox grpAxisY;
         private SpPerfChart.PerfChart perfChart2;
-        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox grpAxisZ;
         private SpPerfChart.PerfChart perfChart3;
-        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox grpCommands;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnCalibrate;
+        private System.Windows.Forms.Label lblZOffset;
+        private System.Windows.Forms.Label lblYOffset;
+        private System.Windows.Forms.Label lblXOffset;
+        private System.Windows.Forms.Label lblPosZ;
+        private System.Windows.Forms.Label lblPosY;
+        private System.Windows.Forms.Label lblPosX;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- AccelMoveTest/frmAccelTest1.cs
+++ AccelMoveTest/frmAccelTest1.cs
@@ -12,26 +12,81 @@ namespace AccelMoveTest
 {
     public partial class frmAccelTest1 : Form
     {
-        USBDevice usb;
+        private USBDevice usb;
+        private bool Calibrated = false;
+        float axisX;
+        float axisY;
+        float axisZ;
+
+        float OffsetX;
+        float OffsetY;
+        float OffsetZ;
+
+        float VelaxisX;
+        float VelaxisY;
+        float VelaxisZ;
+
+        float PosaxisX;
+        float PosaxisY;
+        float PosaxisZ;
+
         public frmAccelTest1()
         {
             InitializeComponent();
             usb = new USBDevice(0x3995, 0x0030);
-            timer1.Enabled = usb.HIDisOpen;
+            tmrReceive.Enabled = usb.HIDisOpen;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             usb.ReciveBuffer();
             usb.StreamReadBegin();
-            float axisX = usb.StreamReadFloat();
-            float axisY = usb.StreamReadFloat();
-            float axisZ = usb.StreamReadFloat();
+            axisX = usb.StreamReadFloat();
+            axisY = usb.StreamReadFloat();
+            axisZ = usb.StreamReadFloat();
 
-            label1.Text = ("X: " + axisX.ToString() + " Y: " + axisY.ToString() +  " Z: " + axisZ.ToString());
+            char WhoHeIs = usb.StreamReadChar();
+            //Calibrated = usb.StreamReadChar()==(char)0x01;
+
+            OffsetX = usb.StreamReadFloat();
+            OffsetY = usb.StreamReadFloat();
+            OffsetZ = usb.StreamReadFloat();
+
+            VelaxisX = usb.StreamReadFloat();
+            VelaxisY = usb.StreamReadFloat();
+            VelaxisZ = usb.StreamReadFloat();
+
+            PosaxisX = usb.StreamReadFloat();
+            PosaxisY = usb.StreamReadFloat();
+            PosaxisZ = usb.StreamReadFloat();
+
+
+            lblRawValues.Text = ("X: " + axisX.ToString() + " Y: " + axisY.ToString() +  " Z: " + axisZ.ToString());
+            lblXOffset.Text = "X Offset: " + OffsetX.ToString();
+            lblYOffset.Text = "Y Offset: " + OffsetY.ToString();
+            lblZOffset.Text = "Z Offset: " + OffsetZ.ToString();
+
+            lblPosX.Text = "X: " + PosaxisX.ToString();
+            lblPosY.Text = "Y: " + PosaxisY.ToString();
+            lblPosZ.Text = "Z: " + PosaxisZ.ToString();
+
             perfChart1.AddValue((int)(axisX * 25) + 50);
             perfChart2.AddValue((int)(axisY * 25) + 50);
             perfChart3.AddValue((int)(axisZ * 25) + 50);
+        }
+
+        private void btnCalibrate_Click(object sender, EventArgs e)
+        {
+            usb.StreamWriteBegin();
+            usb.StreamWriteChar((char)0x01);
+            usb.SendBuffer();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            usb.StreamWriteBegin();
+            usb.StreamWriteChar((char)0x02);
+            usb.SendBuffer();
         }
     }
 }
