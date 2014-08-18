d04596622a6dc26582e43cbf75d38039be318a3f
miguel28 <miguelinho_28@hotmail.com>
2014-08-17 21:02:38 -0600

    Added Class Library to control the quadcoper X
    Added Class Library to control the quadcoper X. This class would be the
    reponsible to control all data transfering between the quadcoper X aand
    the pc host. This library maybe will be ported to Umanaged C++ class
    library.

---
+++ WildDuckLibrary/Reports.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WildDuckLibrary
+{
+    public enum ReportType
+    {
+        Joystick,
+        Sensors,
+        EmergencyLanding,
+        Constans1,
+        Constans2,
+        Constans3
+    }
+    
+    public struct JosytickReport
+    {
+        public char UseTargetMode;
+        public ushort Aileron;
+        public ushort Elevator;
+        public ushort Throttle;
+        public ushort Rudder;
+        public ushort ElevationTarget;
+        public char UChannel;
+    }
+
+    public struct SensorsReport
+    {
+        public char Elevation;
+        public char Front;
+        public char Back;
+        public char Left;
+        public char Right;
+    }
+
+    public struct EmegencyLanding
+    {
+        public char UseEmergencyLanding;
+        public char ConnectionTimeOut;
+        public char BreakOutOffHeight;
+        public ushort DownDecrementCoeficient;
+        public char DecrementTime;
+    }
+
+    public struct Constants1
+    {
+        public char UseProtection;
+        public char ProtectionDistance;
+        public char HS_High_Limit;
+        public char HS_Medium_Limit;
+        public char HS_Low_Limit;
+    }
+
+    public struct Constants2
+    {
+        public ushort HS_UltraHigh_Correction;
+        public ushort HS_High_Correction;
+        public ushort HS_Medium_Correction;
+        public ushort HS_Low_Correction;
+    }
+
+    public struct Reports
+    {
+        public JosytickReport joystickReport;
+        public SensorsReport sensorsReport;
+        public EmegencyLanding emergencyLanding;
+        public Constants1 constant1;
+        public Constants2 constant2;
+    }
+}
---
+++ WildDuckLibrary/WildDuckConnection.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using USBInterface;
+
+namespace WildDuckLibrary
+{
+    public class WildDuckConnection
+    {
+        #region private members
+        private USBDevice usb;
+        private Timer timer;
+        private ReportType _reportType;
+        private const ushort VendorID = 0x3995;
+        private const ushort ProductID = 0x0020;
+        private const int HidReportLength = 16;
+
+        private const int TimesSendCommand = 5;
+        private const int CommandResponseTimeOut = 10;
+        private int ReportQueque = 0;
+        #endregion
+
+        #region Reports
+        private Reports _Received;
+        public Reports Received
+        {
+            get
+            {
+                return _Received;
+            }
+        }
+        public Reports Send;
+        #endregion
+
+        public WildDuckConnection(int pollData = 10)
+        {
+            timer = new Timer();
+            timer.Interval = pollData;
+            timer.Tick += new EventHandler(UpdateDataTransfer);
+
+            _reportType = ReportType.Joystick;
+
+            usb = new USBDevice(VendorID, ProductID, HidReportLength);
+            if(!usb.HIDisOpen)
+                throw new Exception("USB Device Couldn't be opened! Check if Freescale Freedom is connected!");
+        }
+        public void ConnectionStart()
+        {
+            timer.Enabled = true;
+        }
+        public ReportType ReceiveReportType
+        {
+            get
+            {
+                return _reportType;
+            }
+            set
+            {
+                _reportType = value;
+                if (value != ReportType.Joystick)
+                    ReportQueque = TimesSendCommand; 
+            }
+        }
+
+        private void UpdateDataTransfer(object sender, EventArgs e)
+        {
+            SendReport();
+            ReceiveReport();
+        }
+
+        private void SendReport()
+        {
+            ReportType report;
+            if (ReportQueque > 0)
+            {
+                report = _reportType;
+                ReportQueque--;
+            }
+            else report = ReportType.Joystick;
+            
+            switch(report)
+            {
+                case ReportType.Joystick:
+                    SendJoystickReport();
+                    break;
+                case ReportType.Constans1:
+                    break;
+                case ReportType.Constans2:
+                    break;
+                case ReportType.EmergencyLanding:
+                    break;
+            }
+        }
+
+        private void WriteReport(char[] buffer)
+        {
+            usb.StreamWriteBegin();
+            for (int i = 0; i < 10; i++)
+                usb.StreamWriteChar(buffer[i]);
+            usb.SendBuffer();
+        }
+
+        private void SendJoystickReport()
+        {
+            if (Send.joystickReport.Aileron == 255)
+                Send.joystickReport.Aileron = 256;
+
+            if (Send.joystickReport.Elevator == 255)
+                Send.joystickReport.Elevator = 256;
+
+            if (Send.joystickReport.Rudder == 255)
+                Send.joystickReport.Rudder = 256;
+
+            if (Send.joystickReport.Throttle == 255)
+                Send.joystickReport.Throttle = 256;
+
+
+            if (Send.joystickReport.Aileron >= 1023)
+                Send.joystickReport.Aileron = 1023;
+
+            if (Send.joystickReport.Elevator == 1023)
+                Send.joystickReport.Elevator = 1023;
+
+            if (Send.joystickReport.Rudder == 1023)
+                Send.joystickReport.Rudder = 1023;
+
+            if (Send.joystickReport.Throttle == 1023)
+                Send.joystickReport.Throttle = 1023;
+
+            char[] buffer = new char[10];
+            buffer[0] = (char)ReportType.Joystick;
+            buffer[1] = (char)_reportType;
+
+            buffer[2] = (char)(Send.joystickReport.Throttle & 0xff);
+            buffer[3] = (char)(Send.joystickReport.Rudder & 0xff);
+            buffer[4] = (char)(Send.joystickReport.Aileron & 0xff);
+            buffer[5] = (char)(Send.joystickReport.Elevator & 0xff);
+
+            char temp1 = (char)((char)((Send.joystickReport.Throttle >> 8) << 6) & 0xff);
+            char temp2 = (char)((char)((Send.joystickReport.Rudder >> 8) << 4) & 0xff);
+            char temp3 = (char)((char)((Send.joystickReport.Aileron >> 8) << 2) & 0xff);
+            char temp4 = (char)((char)((Send.joystickReport.Elevator >> 8) << 0) & 0xff);
+            char temp5 = (char)((temp1 | temp2 | temp3 | temp4) & 0xff);
+
+            buffer[6] = temp5;
+            buffer[7] = (char)Send.joystickReport.ElevationTarget;
+            buffer[8] = (char)Send.joystickReport.UChannel;
+            buffer[9] = (char)Send.joystickReport.UseTargetMode;
+            
+            WriteReport(buffer);
+        }
+
+        private void ReceiveReport()
+        {
+
+        }
+    }
+}
