bd1cbcd41a67c9cc89b2cc655ec5301c074f591a
miguel28 <miguelinho_28@hotmail.com>
2014-10-23 18:47:19 -0700

    Added license

--- WildDuckIndependientControl/main.h
+++ WildDuckIndependientControl/main.h
@@ -1,3 +1,27 @@
+/******************************************************************
+Wild Duck Independient Control
+Project Created 8/3/2014
+File main.h
+
+This program has been created by using mbed runtime libraries
+for the platform FRDM-KL25Z in a offline project managed by
+arm-gcc compiler.
+
+For more details see (http://www.mbed.org) for a gerneal
+overview. And for the development se (http://developer.mbed.org/) 
+
+*This program is free software: you can redistribute it and/or 
+modify it under the terms of the GNU General Public License as 
+published by the Free Software Foundation, either version 3 of 
+the License, or(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+<http://www.gnu.org/licenses/>
+******************************************************************/
+
 #include "mbed.h"        //// Include mbed main runtime library check www.mbed.org
 #include "Joystick.h"    //// include Joystick class
 #include "LedBar.h"      //// include led bar class
@@ -21,11 +45,11 @@ typedef struct
 
 #define JOY_DEATH_ZONE 0.10f   //// Defined a constant of deathzone for joystick
 #define MAX_AXIS 1.0f          //// Porportional constant of axis
-#define SENSIBILITY 200
+#define SENSIBILITY 200		   //// This is a Sensibility Factor
 #define UPDATE_RATE 0.049f
 
-//#define __DEBUG_PC_
-#define USE_RF
+//#define __DEBUG_PC_  //// This is a flag macro for enables pc serial port debugging
+#define USE_RF         //// This macro flag is for compiles the pertinents instructions
 
 #ifdef __DEBUG_PC_
 Serial pc(USBTX, USBRX);
